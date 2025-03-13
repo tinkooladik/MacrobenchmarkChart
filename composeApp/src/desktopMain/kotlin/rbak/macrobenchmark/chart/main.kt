15a0f6d6dc9a9ca3f47b1930ac2c67de278b4d5e
tinkooladik <tinkooladik@gmail.com>
2025-03-13 11:59:55 -0400

    added build command

--- composeApp/src/desktopMain/kotlin/rbak/macrobenchmark/chart/main.kt
+++ composeApp/src/desktopMain/kotlin/rbak/macrobenchmark/chart/main.kt
@@ -3,6 +3,7 @@ package rbak.macrobenchmark.chart
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+// ./gradlew run desktopRun -DmainClass=MainKt --quiet
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
