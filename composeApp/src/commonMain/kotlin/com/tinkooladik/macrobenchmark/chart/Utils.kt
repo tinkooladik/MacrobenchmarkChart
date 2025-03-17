8f8f848e29a7da23088b4b8d181158e59c56b0e2
Oleksandra Tkachenko <tinkooladik@gmail.com>
2025-03-17 19:18:03 -0400

    Generate report (#14)
    * moved files
    
    * some restructure; capture chart image
    
    * copy report png file to clipboard
    
    * small cleanup

--- composeApp/src/commonMain/kotlin/com/tinkooladik/macrobenchmark/chart/Utils.kt
+++ composeApp/src/commonMain/kotlin/com/tinkooladik/macrobenchmark/chart/Utils.kt
@@ -1,6 +1,14 @@
 package com.tinkooladik.macrobenchmark.chart
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ImageComposeScene
+import androidx.compose.ui.unit.Density
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.json.Json
+import org.jetbrains.skia.Bitmap
+import org.jetbrains.skia.EncodedImageFormat
+import org.jetbrains.skia.Image
+import java.io.File
 
 val json = Json {
     ignoreUnknownKeys = true
@@ -17,3 +25,66 @@ val benchmarkComparisonRules: Map<String, String> = mapOf(
     "frameDurationCpuMs" to "The lower, the better. Measures frame rendering time; lower values mean smoother performance.",
     "frameOverrunMs" to "The lower, the better. Measures frame rendering delays; high values indicate lag and jank."
 )
+
+/** Captures a composable and renders it into an ImageBitmap */
+fun captureComposableAsImage(
+    width: Int,
+    height: Int,
+    // todo let user chose folder
+    folder: String = "reports",
+    // todo this will be benchmark name
+    title: String = "report",
+    content: @Composable () -> Unit
+) {
+    val scene = ImageComposeScene(
+        width = width,
+        height = height,
+        density = Density(3f),
+        coroutineContext = Dispatchers.Unconfined
+    ) {
+        content()
+    }
+
+    val img = scene.render()
+
+    val bitmap = Bitmap.makeFromImage(img)
+    val data = Image.makeFromBitmap(bitmap).use { image ->
+        image.encodeToData(EncodedImageFormat.PNG)
+    }
+    val reportsDir = File(folder)
+    if (!reportsDir.exists()) {
+        reportsDir.mkdirs()
+    }
+    val out = File(reportsDir, "$title.png")
+    out.delete()
+    data?.bytes?.let {
+        out.writeBytes(data.bytes)
+    }
+    copyFileToClipboardMacOS(out)
+}
+
+fun copyFileToClipboardMacOS(file: File) {
+    if (!file.exists()) {
+        println("❌ File not found: ${file.absolutePath}")
+        return
+    }
+
+    try {
+        val appleScript = """
+            set theFile to POSIX file "${file.absolutePath}"
+            set theList to {theFile}
+            set theClipboard to theList as «class furl»
+            set the clipboard to theClipboard
+        """.trimIndent()
+
+        val process = ProcessBuilder("osascript", "-e", appleScript)
+            .redirectErrorStream(true)
+            .start()
+
+        process.waitFor()
+
+        println("✅ File copied to clipboard (macOS): ${file.absolutePath}")
+    } catch (e: Exception) {
+        println("❌ Failed to copy file to clipboard: ${e.message}")
+    }
+}
--- composeApp/src/desktopMain/kotlin/com/tinkooladik/macrobenchmark/chart/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.tinkooladik.macrobenchmark.chart
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
